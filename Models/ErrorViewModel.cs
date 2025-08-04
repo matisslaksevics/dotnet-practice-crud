29e0f85b03d5c15752f01a9defc534d0f2007e53
Matiss Laksevics <matiss.laksevics12@gmail.com>
2025-08-04 14:52:34 +0300

    Basic .NET template generation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetPracticeCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
